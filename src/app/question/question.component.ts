00d32c768ac6b2719494d7d114a9dc94ae040b7b
pavithra <pavithraravi1902@gmail.com>
2022-06-22 19:20:13 +0530

    Quiz-app layout

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../service/question.service';
 
 @Component({
   selector: 'app-question',
@@ -6,11 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
-public name: string ="";
-  constructor() { }
+  public name: string = "";
+  public questionList: any = [];
+  public currentQuestion: number = 0;
+  public points: number = 0;
+  public counter: number = 60;
+  constructor(private service: QuestionService) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
+    this.getAllQuestions();
+  }
+  getAllQuestions() {
+    this.service.getQuestionJson().subscribe(res => {
+      this.questionList = res.questions
+    })
+  }
+  nextQuestion() {
+    this.currentQuestion++;
+  }
+  previousQuestion() {
+    this.currentQuestion--;
   }
 
 }
